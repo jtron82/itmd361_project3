d89194e70e350329ee4f38b5f5878ac61daee4d4
jtron82 <31261926+jtron82@users.noreply.github.com>
2017-11-14 20:08:33 -0600

    updated location
    updated map location

--- gmap.js
+++ gmap.js
@@ -1,11 +1,11 @@
 function initMap() {
-  var uluru = {lat: -25.363, lng: 131.044};
+  var uluru = {lat: 41.942644, lng: -87.702291};
   var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 4,
+    zoom: 10,
     center: uluru
   });
   var marker = new google.maps.Marker({
     position: uluru,
-    map : map
+    map: map
   });
 }
