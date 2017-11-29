8315b75f5af5ef2f8958da1f06f8b8a14964fb38
jtron82 <31261926+jtron82@users.noreply.github.com>
2017-11-28 20:16:15 -0600

    fixed indentation in js
    fixed unnecessary indentations in js

--- gmap.js
+++ gmap.js
@@ -12,11 +12,11 @@ function initMap() {
   });
   var info = "<h1>Honey Butter Fried Chicken</h1><p>In an origin story that's somehow better than Wolverine's, the folks behind Sunday Dinner Club happened upon a marvelous recipe when, at a supper six years ago, a tray of honey-buttered corn cakes happened to spill over on a plate of fried chicken. Now the happy accident is the namesake of their new brick 'n mortar restaurant representing possibly the four most beautiful words in the English language: Honey Butter Fried Chicken.</p>";
 
-	var infobox = new google.maps.InfoWindow({
-      content: info
-  	});
+  var infobox = new google.maps.InfoWindow({
+    content: info
+  });
 
-	google.maps.event.addListener(marker, 'mouseover', function() {
-    	infobox.open(map, marker);
-  	});
+  google.maps.event.addListener(marker, 'mouseover', function() {
+    infobox.open(map, marker);
+  });
 }
